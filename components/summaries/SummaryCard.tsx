32f78bb4d9afa26d678aa4bdae344c0df4c66661
Nifemi Oladapo <nifethedev@gmail.com>
2026-06-25 01:20:48 +0100

    added the formated dating calc func

--- components/summaries/SummaryCard.tsx
+++ components/summaries/SummaryCard.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link';
 import DeleteButton from './DeleteButton';
 import { FileText } from 'lucide-react';
 import { cn } from '@/lib/utils';
+import { formatDistanceToNow } from "date-fns";
+import { formatFileNameAsTitle } from '@/lib/format-utils';
 
 const SummaryHeader = ({ fileUrl, title, createdAt }:
     { fileUrl: string, title: string | null, createdAt: string }) => {
@@ -10,9 +12,9 @@ const SummaryHeader = ({ fileUrl, title, createdAt }:
         <FileText className="w-6 h-6 sm:w-8 sm:h-8 text-rose-400 mt-1" />
         <div className="flex-1 min-w-0">
             <h3 className="text-base xl:text-lg font-semibold text-gray-900 truncate w-4/5">
-                {title}
+                {title || formatFileNameAsTitle(fileUrl)}
             </h3>
-            <p className="text-sm text-gray-500">{new Date(createdAt).toLocaleDateString()}</p>
+            <p className="text-sm text-gray-500">{formatDistanceToNow(new Date(createdAt), { addSuffix: true })}</p>
         </div>
     </div>
 }
